e4f3f05edf2594b9366b578d25ddebcfd46d41a0
Beppe Catanese <gcatanese@yahoo.com>
2022-03-01 17:58:56 +0100

    Add command and repo size

--- src/main/java/com/tweesky/cloudtools/model/AppInfo.java
+++ src/main/java/com/tweesky/cloudtools/model/AppInfo.java
@@ -1,5 +1,7 @@
 package com.tweesky.cloudtools.model;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class AppInfo {
@@ -8,15 +10,35 @@ public class AppInfo {
     private String web_url;
     private String region;
     private int repo_size;
+    private String command;
 
     public AppInfo(String json) {
+
+        if(!isValidJson(json)) {
+            throw new RuntimeException("Invalid json");
+        }
+
         JSONObject myJson = new JSONObject(json);
 
         this.setName(myJson.getJSONObject("app").getString("name"));
         this.setRegion(myJson.getJSONObject("app").getJSONObject("region").getString("name"));
         this.setWeb_url(myJson.getJSONObject("app").getString("web_url"));
         this.setRepo_size(myJson.getJSONObject("app").getInt("repo_size"));
+        this.setCommand(myJson.getJSONArray("dynos").getJSONObject(0).getString("command"));
 
+    }
+
+    boolean isValidJson(String json) {
+        try {
+            new JSONObject(json);
+        } catch (JSONException ex) {
+            try {
+                new JSONArray(json);
+            } catch (JSONException ex1) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public String getName() {
@@ -49,5 +71,13 @@ public class AppInfo {
 
     public void setRepo_size(int repo_size) {
         this.repo_size = repo_size;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
     }
 }
